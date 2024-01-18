d306f483c899106cf402cfc2c87cacf990d516df
Eric Lindgren <nephilim.eric@gmail.com>
2024-01-18 00:55:40 -0700

    updates flyer to pdf

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
         return [
             {
                 source: '/flyer',
-                destination: '/flyer.html',
+                destination: '/flyer.pdf',
             },
         ]
     },
